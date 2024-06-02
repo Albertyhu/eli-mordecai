074a06166360f279b8301598350ec431391c6bee
Albertyhu <hualbert@rocketmail.com>
2024-06-02 01:09:47 -0700

    finished with contact form

---
+++ src/components/MessageComponent/index.tsx
@@ -0,0 +1,21 @@
+import "./style.css";
+
+type MessageProps = {
+  message: string;
+  messageRef: any;
+};
+
+const MessageComponent = (props: MessageProps) => {
+  const { message, messageRef } = props;
+
+  return (
+    <div
+      ref={messageRef}
+      className="font-primaryF bg-black border-2 border-primaryC border-solid rounded-lg text-white text-center h-fit w-11/12 sm:w-8/12 md:w-[300px] fixed top-[25%] left-[50%] translate-x-[-50%] flex justify-center transition-all duration-500 z-20 py-5 MessageFadeOut"
+    >
+      {message}
+    </div>
+  );
+};
+
+export default MessageComponent;
--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,16 +1,18 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, type ReactHTMLElement } from "react";
 import PracticeArea from "@/data/practice-areas.js";
 import uuid from "react-uuid";
+import emailjs from "@emailjs/browser";
+import { BusinessInfo } from "@/data/business-info";
+import MessageComponent from "./MessageComponent";
 
 const PUBLIC_KEY = import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY;
 const TEMPLATE_KEY = import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID;
 const SERVICE_ID = import.meta.env.PUBLIC_EMAILJS_SERVICE_ID;
 
-const ContactFromComponent = (props) => {
-  const {} = props;
+const ContactFromComponent = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
+  const [phone, setPhone] = useState<number | null>();
   const [service, setService] = useState<string>("");
   const [message, setMessage] = useState<string>("");
 
@@ -22,7 +24,7 @@ const ContactFromComponent = (props) => {
     setName(evt.target.value);
   };
 
-  const handlePhone = (evt: any) => {
+  const handlePhone = (evt: any | null) => {
     setPhone(evt.target.value);
   };
 
@@ -34,6 +36,54 @@ const ContactFromComponent = (props) => {
     setMessage(evt.target.value);
   };
 
+  const reset = () => {
+    setName("");
+    setEmail("");
+    setPhone(null);
+    setService("");
+    setMessage("");
+  };
+
+  const messageRef = useRef<HTMLElement>(null);
+
+  const DisplayMessage = () => {
+    if (messageRef.current) {
+      messageRef?.current?.classList.remove("MessageFadeOut");
+      setTimeout(() => {
+        messageRef?.current?.classList.add("MessageFadeOut");
+      }, 2000);
+    }
+  };
+
+  const HandleSubmit = (evt: any) => {
+    evt.preventDefault();
+
+    let templateParams = {
+      to_name: BusinessInfo.owner,
+      from_name: name,
+      subject_line: `Someone sent you a new message through ${BusinessInfo.websiteURL}`,
+      service,
+      phone_number: phone,
+      message: message,
+      reply_to: email,
+    };
+    emailjs.send(SERVICE_ID, TEMPLATE_KEY, templateParams, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+        reset();
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+        DisplayMessage();
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <form
       id="email-form"
@@ -41,9 +91,12 @@ const ContactFromComponent = (props) => {
       data-name="Email Form"
       method="get"
       className="contact-form"
-      data-wf-page-id="663690b06ff2152d9a1b9e1c"
-      data-wf-element-id="578df7e1-f4ff-e7a7-6436-8847b1666e0e"
+      onSubmit={HandleSubmit}
     >
+      <MessageComponent
+        message="Your message has been sent."
+        messageRef={messageRef}
+      />
       <div className="contact-input-fields">
         <div className="contact-form-left-wrap">
           <div className="input-wrap">
@@ -128,17 +181,6 @@ const ContactFromComponent = (props) => {
         </div>
       </div>
       <div className="input-wrap">
-        {/* <input
-          className="input-field text-area w-input"
-          maxLength={256}
-          name="Your-Message"
-          data-name="Your Message"
-          placeholder="Your Message"
-          type="text"
-          id="Your-Message"
-          value={message}
-          onChange={handleMessage}
-        /> */}
         <textarea
           onChange={handleMessage}
           className="input-field text-area w-input resize-none"
@@ -153,7 +195,7 @@ const ContactFromComponent = (props) => {
         <input
           type="submit"
           data-wait="Please wait..."
-          className="button-large w-button"
+          className="button-large w-button duration-500"
           value="Send Message"
         />
       </div>
